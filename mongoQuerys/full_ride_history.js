88639a6447ac1b6acd91ccff7d5088806c1c8b1a
munkhuu <monhood34@gmail.com>
2019-05-06 22:06:43 +0800

    final

--- mongoQuerys/full_ride_history.js
+++ mongoQuerys/full_ride_history.js
@@ -1,11 +1,13 @@
 db.getCollection('ride_history').aggregate([
                             {
                             $match :{ created_at: {
-                                $gte: ISODate("2018-10-01T00:00:00.000Z"),
-                                $lte: ISODate("2018-10-21T00:00:00.000Z"),
-                            }}},
+                                $gte: ISODate("2018-09-01T00:00:00.000Z"),
+                                $lte: ISODate("2019-04-30T00:00:00.000Z"),
+                            },
+                            passengers: { $exists: true, $not: {$size: 0} }},
+                            },
                             {                           
-                            $group:{ _id: '$driver._id',
+                            $group:{ _id: null,
                                total_km: {
                                         $sum: '$total_distance',
                                     },
@@ -14,4 +16,9 @@ db.getCollection('ride_history').aggregate([
                                     },
                                 }
                             }
-                     ])
+
+db.getCollection('ride_history').count({ created_at: {
+                                $gte: ISODate("2018-09-01T00:00:00.000Z"),
+                                $lte: ISODate("2019-04-30T00:00:00.000Z"),
+                            },
+                            passengers: { $exists: true, $not: {$size: 0} }},{"_id":1})
